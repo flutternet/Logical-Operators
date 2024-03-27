d23c1f84de1707c685006ac6690eb73e06a18d09
flutternet <163728706+flutternet@users.noreply.github.com>
2024-03-27 01:25:14 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+//Logical Operators(&&=and,||=or,!=not):
+var a = 6;
+var b = 4;
+//AND Operator:
+var And = (a > b && b < a);
+console.log(And);
+//OR Operator:
+var OR = (a < b || a > b);
+console.log(OR);
+//Not Operator:
+var Not = !(a == b);
+console.log(Not);
---
+++ main.ts
@@ -0,0 +1,12 @@
+//Logical Operators(&&=and,||=or,!=not):
+let a=6
+let b=4
+//AND Operator:
+let And=(a>b && b<a); 
+console.log(And);
+//OR Operator:
+let OR=(a<b || a>b)
+console.log(OR);
+//Not Operator:
+let Not=!(a==b)
+console.log(Not);
